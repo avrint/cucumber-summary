2855ffcfb01edd46838dba29cf3a7c317830b04c
Hansel <djhansel@gmail.com>
2026-08-26 16:01:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -303,10 +303,10 @@ function renderGlobalSummary (stats) {
   if (failedCount > 0) summaryText += `${summaryText ? ", " : ""}${failedCount} failed`;
   if (skippedCount > 0) summaryText += `${summaryText ? ", " : ""}${skippedCount} skipped`;
 
-  md.addRaw('<div align="center" width="400">');
+  md.addRaw('<div align="center" width="400">\n\n');
   md.addRaw(generateCucumberChart(stats, 'Cucumber Results'));
-  md.addRaw('</div>');
-  
+  md.addRaw('\n\n</div>\n\n');
+
   if (duration) {
     md.addRaw(`\n⏱ **Duration:** ${duration}\n`);
   }
